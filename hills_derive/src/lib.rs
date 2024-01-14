c3a68ee1e35f7edc4df55dc92f73f1f8abf32187
Roman Isaikin <romix.lab@gmail.com>
2024-01-14 11:56:00 +0100

    Reflect: implement enum support.

--- hills_derive/src/lib.rs
+++ hills_derive/src/lib.rs
@@ -1,31 +1,32 @@
 use proc_macro::TokenStream;
 use proc_macro_error::__export::proc_macro2::Span;
 use proc_macro_error::abort;
-use quote::{quote, TokenStreamExt, ToTokens};
-use syn::{parse_macro_input, DeriveInput, Data, Attribute, Type};
+use quote::{quote, ToTokens, TokenStreamExt};
 use syn::spanned::Spanned;
+use syn::{parse_macro_input, Attribute, Data, DeriveInput, Fields, Type};
 
 #[proc_macro_derive(Reflect)]
 pub fn reflect_fn(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
-    match input.span() { Span { .. } => {} }
+    match input.span() {
+        Span { .. } => {}
+    }
     if input.generics.lt_token.is_some() {
         abort!(input.generics.span(), "Generics are not supported");
     }
     // eprintln!("{:?}", input.attrs);
     let doc = collect_docs(&input.attrs);
-    let mut ts_non_std = quote!();
+    let mut non_std_types = Vec::new();
 
-    let ts = match input.data {
+    let reflect_ts = match input.data {
         Data::Struct(ds) => {
             let mut ts = quote!(
                 let mut fields = Vec::new();
             );
-            let mut non_std_types = Vec::new();
             for (idx, f) in ds.fields.into_iter().enumerate() {
-                let name = match f.ident {
-                    Some(ident) => { ident.to_string() },
-                    None => idx.to_string()
+                let ident = match f.ident {
+                    Some(ident) => ident.to_string(),
+                    None => idx.to_string(),
                 };
                 let ty = ty_to_str(&f.ty);
                 if !STD_TYPES.contains(&ty.as_str()) {
@@ -34,8 +35,8 @@ pub fn reflect_fn(input: TokenStream) -> TokenStream {
                     }
                 }
                 ts.append_all(quote!(
-                    fields.push(hills_base::StructField{
-                        name: #name.to_string(),
+                    fields.push(hills_base::StructField {
+                        ident: #ident.to_string(),
                         ty: #ty.to_string(),
                     });
                 ));
@@ -47,21 +48,86 @@ pub fn reflect_fn(input: TokenStream) -> TokenStream {
                 });
             ));
 
-            for ty in non_std_types {
-                ts_non_std.append_all(quote!(
-                    #ty::reflect(to);
-                ));
-            }
             ts
         }
         Data::Enum(de) => {
-            quote!()
+            let mut ts = quote!(
+                let mut variants = Vec::new();
+            );
+
+            for variant in de.variants.iter() {
+                let fields = match &variant.fields {
+                    Fields::Named(fields_named) => {
+                        let mut idents = Vec::new();
+                        let mut tys = Vec::new();
+                        for (idx, f) in fields_named.named.iter().enumerate() {
+                            let ty = ty_to_str(&f.ty);
+                            let ident = match &f.ident {
+                                Some(ident) => ident.to_string(),
+                                None => idx.to_string(),
+                            };
+                            idents.push(ident);
+                            if !STD_TYPES.contains(&ty.as_str()) {
+                                if let Type::Path(p) = &f.ty {
+                                    non_std_types.push(p.path.clone());
+                                }
+                            }
+                            tys.push(ty);
+                        }
+                        quote! { hills_base::EnumFields::Named([
+                            #( hills_base::StructField { ident: #idents.to_string(), ty: #tys.to_string() } ),*
+                        ].into()) }
+                    }
+                    Fields::Unnamed(fields_unnamed) => {
+                        let mut list = Vec::new();
+                        for f in &fields_unnamed.unnamed {
+                            let ty = ty_to_str(&f.ty);
+                            if !STD_TYPES.contains(&ty.as_str()) {
+                                if let Type::Path(p) = &f.ty {
+                                    non_std_types.push(p.path.clone());
+                                }
+                            }
+                            list.push(ty);
+                        }
+
+                        quote!(hills_base::EnumFields::Unnamed([
+                            #(#list.to_string()),*
+                        ].into()))
+                    }
+                    Fields::Unit => {
+                        quote!(hills_base::EnumFields::Unit)
+                    }
+                };
+                let ident = variant.ident.to_string();
+                ts.append_all(quote!(
+                    variants.push(hills_base::EnumVariant {
+                        ident: #ident.to_string(),
+                        fields: #fields
+                    });
+                ));
+            }
+
+            ts.append_all(quote!(
+                let self_reflect = hills_base::TypeInfo::Enum(hills_base::EnumInfo {
+                    doc: #doc.to_string(),
+                    variants
+                });
+            ));
+
+            ts
         }
         Data::Union(_) => {
             abort!(input.span(), "Unions are not supported");
         }
     };
     // eprintln!("{ts}");
+
+    let mut reflect_non_std_ts = quote!();
+    for ty in non_std_types {
+        reflect_non_std_ts.append_all(quote!(
+            #ty::reflect(to);
+        ));
+    }
 
     let struct_ident = input.ident;
     let struct_ident_str = struct_ident.to_string();
@@ -72,12 +138,13 @@ pub fn reflect_fn(input: TokenStream) -> TokenStream {
                 if to.root.is_empty() {
                     to.root = ty_name.clone();
                 }
-                #ts
+                #reflect_ts
                 to.refs.insert(ty_name, self_reflect);
-                #ts_non_std
+                #reflect_non_std_ts
             }
         }
-    ).into()
+    )
+    .into()
 }
 
 fn collect_docs(attrs: &[Attribute]) -> String {
@@ -91,11 +158,8 @@ fn collect_docs(attrs: &[Attribute]) -> String {
 }
 
 const STD_TYPES: [&str; 17] = [
-    "u8", "u16", "u32", "u64", "u128",
-    "i8", "i16", "i32", "i64", "i128",
-    "f32", "f64",
-    "bool",
-    "String", "Vec", "HashMap", "HashSet"
+    "u8", "u16", "u32", "u64", "u128", "i8", "i16", "i32", "i64", "i128", "f32", "f64", "bool",
+    "String", "Vec", "HashMap", "HashSet",
 ];
 
 fn ty_to_str(ty: &Type) -> String {
--- hills_derive/tests/basic.rs
+++ hills_derive/tests/basic.rs
@@ -1,44 +1,93 @@
+use hills_base::{EnumFields, Reflect, StructField, TypeCollection, TypeInfo};
 use hills_derive::Reflect;
-use hills_base::{Reflect, TypeCollection, TypeInfo};
 
 #[derive(Reflect)]
-struct Simple {
+struct MyStruct {
     _x: u32,
     _y: u32,
-    _z: NonStandard
+    _z: NonStandard,
 }
 
 #[derive(Reflect)]
 struct NonStandard {
-    _z: u32
+    _z: u32,
+}
+
+#[derive(Reflect)]
+enum MyEnum {
+    _A,
+    _B(i32, f32),
+    _C { x: u8, y: u16 },
 }
 
 #[test]
-fn basic_test() {
-    let mut ty_info = TypeCollection::new();
-    Simple::reflect(&mut ty_info);
-    // println!("{ty_info:#?}");
-    let simple = ty_info.refs.get("basic::Simple").unwrap();
-    assert!(matches!(simple, TypeInfo::Struct(_)));
-    if let TypeInfo::Struct(s) = simple {
+fn struct_test() {
+    let mut tc = TypeCollection::new();
+    MyStruct::reflect(&mut tc);
+    // println!("{tc:#?}");
+    assert_eq!(tc.root, "basic::MyStruct");
+    let my_struct = tc.refs.get("basic::MyStruct").unwrap();
+    assert!(matches!(my_struct, TypeInfo::Struct(_)));
+    if let TypeInfo::Struct(s) = my_struct {
         let mut fields = s.fields.iter();
         let field_x = fields.next().unwrap();
-        assert_eq!(field_x.name, "_x");
+        assert_eq!(field_x.ident, "_x");
         assert_eq!(field_x.ty, "u32");
         let field_y = fields.next().unwrap();
-        assert_eq!(field_y.name, "_y");
+        assert_eq!(field_y.ident, "_y");
         assert_eq!(field_y.ty, "u32");
         let field_z = fields.next().unwrap();
-        assert_eq!(field_z.name, "_z");
+        assert_eq!(field_z.ident, "_z");
         assert_eq!(field_z.ty, "NonStandard");
     }
 
-    let non_standard = ty_info.refs.get("basic::NonStandard").unwrap();
+    let non_standard = tc.refs.get("basic::NonStandard").unwrap();
     assert!(matches!(non_standard, TypeInfo::Struct(_)));
     if let TypeInfo::Struct(s) = non_standard {
         let mut fields = s.fields.iter();
         let field_z = fields.next().unwrap();
-        assert_eq!(field_z.name, "_z");
+        assert_eq!(field_z.ident, "_z");
         assert_eq!(field_z.ty, "u32");
+    }
+}
+
+#[test]
+fn enum_test() {
+    let mut tc = TypeCollection::new();
+    MyEnum::reflect(&mut tc);
+    // println!("{tc:#?}");
+    let my_enum = tc.refs.get("basic::MyEnum").unwrap();
+    assert!(matches!(my_enum, TypeInfo::Enum(_)));
+    if let TypeInfo::Enum(e) = my_enum {
+        let mut variants = e.variants.iter();
+        let variant0 = variants.next().unwrap();
+        assert_eq!(variant0.ident, "_A");
+        assert_eq!(variant0.fields, EnumFields::Unit);
+
+        let variant1 = variants.next().unwrap();
+        assert_eq!(variant1.ident, "_B");
+        assert_eq!(
+            variant1.fields,
+            EnumFields::Unnamed(["i32".into(), "f32".into()].into())
+        );
+
+        let variant2 = variants.next().unwrap();
+        assert_eq!(variant2.ident, "_C");
+        assert_eq!(
+            variant2.fields,
+            EnumFields::Named(
+                [
+                    StructField {
+                        ident: "x".into(),
+                        ty: "u8".to_string()
+                    },
+                    StructField {
+                        ident: "y".into(),
+                        ty: "u16".to_string()
+                    }
+                ]
+                .into()
+            )
+        )
     }
 }
